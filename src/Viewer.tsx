8812913d718f6f4245462480c7fc9dfaa9c9a28b
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2024-05-28 15:41:50 +0300

    some clean

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -1,35 +1,16 @@
 'use client';
-import Play from '@/../public/icons/playButton.svg';
+
 import { Logo } from '@/components/ui/Logo';
 import { Button } from './components/ui/Button';
 
 export const Viewer = () => {
   return (
     <>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
-        nemo possimus, ipsa hic dolores commodi incidunt modi labore aliquam?
-        Repellendus explicabo architecto dolore asperiores nam earum ipsum.
-        Ipsum dolorem dicta sunt libero molestias voluptatibus necessitatibus
-        incidunt non facilis tenetur vero eos rem fuga laudantium aspernatur,
-        distinctio cupiditate itaque est, explicabo neque magni ea! Incidunt
-        cumque molestiae illum odio! Culpa assumenda accusantium cupiditate hic
-        quasi quia aperiam qui, voluptatum incidunt? Similique unde tempora,
-        modi rem quasi fugiat. Nam totam perferendis iure veniam quos quis
-        deleniti laudantium aliquid natus? Soluta?
-      </p>
-
-      <button
-        type='button'
-        className='h-[51px] w-[51px] rounded-full bg-orange-600 pb-4 pl-[21px] pr-[17px] pt-[17px]'
-      >
-        <Play width={13} height={18} />
-      </button>
       {/* div тимчасово, щоб було видно логотип */}
       <div className='h-[51px] w-full bg-[#1A341B]'>
         <Logo />
       </div>
-      <div className='h-[500px] w-full bg-darkGrey'>
+      <div className='bg-darkGrey h-[100px] w-full'>
         <Button
           text='Запит на підтримку'
           handleClick={() => console.log('click')}
