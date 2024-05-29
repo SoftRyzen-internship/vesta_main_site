2662d204ba71c3b4199eec973a86af406a42153b
Alina-Pishak <alinapishak@icloud.com>
2024-05-29 16:54:21 +0300

    add dwelling card

---
+++ src/components/common/DwellingCard/DwellingCard.types.ts
@@ -0,0 +1,4 @@
+export interface IDwellingCardProps {
+  city: string;
+  contacts: { link: string; text: string; target?: string; rel?: string }[];
+}
--- src/data/index.ts
+++ src/data/index.ts
@@ -2,3 +2,4 @@ export { default as metaData } from './metaData.json';
 export { default as logoData } from './logoData.json';
 export { default as socialMedias } from './socialMedias.json';
 export { default as service } from './service.json';
+export { default as dwellings } from './dwellings.json';
