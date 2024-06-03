f159589f67888a1cf3c6efcb746334a2549a1585
Violeta <vnaboishchykova@gmail.com>
2024-06-03 09:58:20 +0200

    added_blur

--- src/components/common/BurgerMenu/BurgerMenu.tsx
+++ src/components/common/BurgerMenu/BurgerMenu.tsx
@@ -16,27 +16,36 @@ export const BurgerMenu: FC<BurgerMenuProps> = ({ isOpen, close }) => {
   return (
     <>
       <Dialog open={isOpen} onClose={close} className='relative'>
-        <DialogPanel className='fixed right-0 top-0 h-screen w-screen bg-green px-[16px] pb-[118px] pt-[84px] md:h-[592px] md:w-[360px] md:px-[40px] md:pb-[70px] md:pt-[78px]'>
-          <div className='absolute left-[25px] top-[25px] md:hidden'>
-            <Logo />
-          </div>
-          <Button
-            variant='closeMenu'
-            handleClick={close}
-            className='absolute right-[25px] top-[25px]'
-          />
-          <div className='flex h-full flex-col items-start justify-between'>
-            <div className='flex flex-col items-start gap-[24px]'>
-              <NavList navList={navListData} forFooter={false} click={close} />
-              <NavLink
-                text='Контакти'
-                href='/contacts'
-                variant='contactsHeader'
-              />
+        <div
+          className='fixed inset-0 bg-[#30303640] backdrop-blur-md'
+          aria-hidden='true'
+        >
+          <DialogPanel className='fixed right-0 top-0 h-screen w-screen bg-green px-[16px] pb-[118px] pt-[84px] md:h-[592px] md:w-[360px] md:px-[40px] md:pb-[70px] md:pt-[78px]'>
+            <div className='absolute left-[25px] top-[25px] md:hidden'>
+              <Logo />
             </div>
-            <SocialMedia forFooter={true} socialMedia={socialMedia} />
-          </div>
-        </DialogPanel>
+            <Button
+              variant='closeMenu'
+              handleClick={close}
+              className='absolute right-[25px] top-[25px]'
+            />
+            <div className='flex h-full flex-col items-start justify-between'>
+              <div className='flex flex-col items-start gap-[24px]'>
+                <NavList
+                  navList={navListData}
+                  forFooter={false}
+                  click={close}
+                />
+                <NavLink
+                  text='Контакти'
+                  href='/contacts'
+                  variant='contactsHeader'
+                />
+              </div>
+              <SocialMedia forFooter={true} socialMedia={socialMedia} />
+            </div>
+          </DialogPanel>
+        </div>
       </Dialog>
     </>
   );
