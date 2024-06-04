7063e91017f6295280e4bbbc39e549219dd6444c
Ljuzifer <Ljuzifer@gmail.com>
2024-06-04 13:37:59 +0300

    donation section done

--- src/sections/Donation/Donation.tsx
+++ src/sections/Donation/Donation.tsx
@@ -12,14 +12,21 @@ export const Donation: FC = () => {
   const { title, description, tomorrow, donateButton, rel, target, monobanka } =
     donationData;
   return (
-    <section className={clsx(s.donation, 'pb-10 pt-[168px]')}>
+    <section
+      className={clsx(
+        s.donation,
+        'pb-10 pt-[168px] md:pb-[60px] md:pt-[282px] xl:pb-[45px] xl:pt-[346px]',
+      )}
+    >
       <div className='container'>
-        <div className='border-t-[0.5px] border-solid border-bgText pt-[15px]'>
-          <h2 className='subtitle pb-5 text-h2 font-bold text-bgText'>
+        <div className='relative border-t-[0.5px] border-solid border-bgText pt-[15px]'>
+          <h2 className='subtitle pb-5 text-h2 font-bold text-bgText md:text-h2_tab xl:text-h2_desk'>
             {title}
           </h2>
-          <p className='pb-5 font-kyiv text-body3 text-bgText'>{description}</p>
-          <p className='text-tomorrow pb-10 text-body1 line-through'>
+          <p className='pb-5 font-kyiv text-body3 text-bgText md:w-[334px] md:pb-10 md:text-body3_tab xl:w-[370px] xl:text-body3_desk'>
+            {description}
+          </p>
+          <p className='text-tomorrow text-body1 line-through smOnly:pb-10 md:absolute md:right-0 md:top-[15px] md:text-body1_tab xl:text-body1_desk'>
             {tomorrow}
           </p>
           <LinkButton
@@ -27,7 +34,7 @@ export const Donation: FC = () => {
             text={donateButton}
             rel={rel}
             target={target}
-            className='w-full'
+            className='w-full md:w-[240px]'
           />
         </div>
       </div>
