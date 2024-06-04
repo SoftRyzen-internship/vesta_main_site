eeea3f90e08248d8d2dc5a12d00f4c79ca27769e
Alina-Pishak <alinapishak@icloud.com>
2024-06-04 15:23:26 +0300

    add projects section

--- src/sections/MainProjects/MainProjects.tsx
+++ src/sections/MainProjects/MainProjects.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 
-import { NavLink } from '@/components/ui/Link';
+import { LinkButton } from '@/components/ui/LinkButton';
 
 import { mainProjectsData } from '@/data';
 import { getSpecialWords } from '@/utils';
@@ -18,29 +18,32 @@ export const MainProjects = () => {
   return (
     <section className='container py-[60px] xl:flex xl:items-center xl:gap-[104px]'>
       <div className='xl:w-[549px]'>
-        <h2 className='font-kyiv text-h3 text-green md:text-h3_tab xl:text-h3_desk mb-[30px] font-bold xl:mb-5'>
-          {getSpecialWords(title, 0, 4)}&nbsp;
-          <span className='bg-yellowLight'>{getSpecialWords(title, 4, 4)}</span>
-          &nbsp;{getSpecialWords(title, 8, 5)}
+        <h2 className='mb-[30px] font-kyiv text-h3 font-bold text-green md:text-h3_tab mdOnly:w-[598px] xl:mb-5 xl:text-h3_desk'>
+          {getSpecialWords(title, 0, 4, { end: true })}
+          <span className=' bg-yellowLight'>
+            {getSpecialWords(title, 4, 4)}
+          </span>
+          {getSpecialWords(title, 8, 5, { start: true })}
         </h2>
-        <p className='paragraph text-darkGrey mb-[30px] md:mb-10 md:w-[415px] xl:mb-10'>
+        <p className='paragraph mb-[30px] text-darkGrey md:mb-10 md:w-[415px] xl:mb-10'>
           {description}
         </p>
-        <NavLink
+        <LinkButton
           text={text}
           href={link}
-          className='hidden md:mb-10 md:inline-block xl:mb-0'
+          className='mb-[30px] smOnly:w-full md:mb-10 md:inline-block xl:mb-0'
         />
       </div>
       <div>
         <Image
           src={img}
-          width={328}
-          height={260}
+          width={0}
+          height={0}
           alt={imgAlt}
-          className=' mb-[30px] h-[260px] w-full object-cover md:h-[470px] xl:mb-0 xl:h-[470px] xl:w-[562px]'
+          sizes='100vw'
+          priority={true}
+          className=' h-[260px] w-full object-cover md:h-[470px]  xl:h-[470px] xl:w-[562px]'
         />
-        <NavLink text={text} href={link} className='w-full md:hidden' />
       </div>
     </section>
   );
--- src/utils/getSpecialWords.ts
+++ src/utils/getSpecialWords.ts
@@ -2,6 +2,13 @@ export const getSpecialWords = (
   text: string,
   start: number,
   count: number,
+  addSpace?: { start?: boolean; end?: boolean },
 ): string => {
-  return text.split(' ').splice(start, count).join(' ');
+  const result = text.split(' ').splice(start, count);
+  if (addSpace && addSpace.start) {
+    result.unshift(' ');
+  } else if (addSpace && addSpace.end) {
+    result.push(' ');
+  }
+  return result.join(' ');
 };
