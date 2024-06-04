dcaccf138ee3bb3813e0a3b6b4c85e12ee4e6e6f
Ljuzifer <Ljuzifer@gmail.com>
2024-06-04 03:20:01 +0300

    add bg-image for mob

--- src/sections/Donation/Donation.tsx
+++ src/sections/Donation/Donation.tsx
@@ -1,28 +1,35 @@
-import { LinkButton } from '@/components/ui/LinkButton';
-import { donationData } from '@/data';
 import { FC } from 'react';
 
+import { clsx } from 'clsx';
+
+import { LinkButton } from '@/components/ui/LinkButton';
+
+import { donationData } from '@/data';
+
+import s from './Donation.module.css';
+
 export const Donation: FC = () => {
+  const { title, description, tomorrow, donateButton, rel, target, monobanka } =
+    donationData;
   return (
-    <section className='pb-10 pt-[168px]'>
-      <div className='container border-t-[0.5px] border-solid border-white pb-[15px]'>
-        <hr className='text-bgText' />
-        <h2 className='subtitle pb-5 text-h2 font-bold text-bgText'>
-          {donationData.title}
-        </h2>
-        <p className='pb-5 text-body3 text-bgText'>
-          {donationData.description}
-        </p>
-        <p className='text-tomorrow pb-10 text-body1 line-through'>
-          {donationData.tomorrow}
-        </p>
-        <LinkButton
-          href={donationData.monobanka}
-          text={donationData.donateButton}
-          rel={donationData.rel}
-          target={donationData.target}
-          className='w-full'
-        />
+    <section className={clsx(s.donation, 'pb-10 pt-[168px]')}>
+      <div className='container'>
+        <div className='border-t-[0.5px] border-solid border-bgText pt-[15px]'>
+          <h2 className='subtitle pb-5 text-h2 font-bold text-bgText'>
+            {title}
+          </h2>
+          <p className='pb-5 font-kyiv text-body3 text-bgText'>{description}</p>
+          <p className='text-tomorrow pb-10 text-body1 line-through'>
+            {tomorrow}
+          </p>
+          <LinkButton
+            href={monobanka}
+            text={donateButton}
+            rel={rel}
+            target={target}
+            className='w-full'
+          />
+        </div>
       </div>
     </section>
   );
