7ab32ab476c9394f7c21d6db1c30ff535f411a65
Ljuzifer <Ljuzifer@gmail.com>
2024-06-04 19:26:25 +0300

    create contacts section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import { Viewer } from '@/Viewer';
 
 import { MainHero } from '@/sections/MainHero';
 import { Donation } from '@/sections/Donation';
+import { Contacts } from '@/sections/Contacts';
 
 export default function Home() {
   return (
     <>
       <MainHero />
       <Donation />
+      <Contacts />
+
       <Viewer />
     </>
   );
---
+++ src/sections/Contacts/Contacts.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export const Contacts: FC = () => {
+  return (
+    <section className=''>
+      <div className='container'></div>
+    </section>
+  );
+};
