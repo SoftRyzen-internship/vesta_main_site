5637e960054101d7f1263db0f5b703e95bc01941
Violeta <vnaboishchykova@gmail.com>
2024-06-05 00:01:15 +0200

    added_variations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import { Viewer } from '@/Viewer';
 
 import { MainHero } from '@/sections/MainHero';
 import { Donation } from '@/sections/Donation';
+import { Partners } from '@/sections/Partners';
 
 export default function Home() {
   return (
     <>
       <MainHero />
       <Donation />
+      <Partners />
       <Viewer />
     </>
   );
--- src/sections/Partners/Partners.tsx
+++ src/sections/Partners/Partners.tsx
@@ -1,17 +1,33 @@
+'use client';
+
 import { FC } from 'react';
+import { usePathname } from 'next/navigation';
 
 import { PartnerCard } from '@/components/common/PartnerCard';
 
+import { useWindowSize } from '@/utils/getWindowSize';
 import { partners } from '@/data';
 
 export const Partners: FC = () => {
+  const pathName = usePathname();
+  const sizes = useWindowSize();
+  const numberOfCards =
+    pathName === '/about' ? 12 : sizes.width && sizes.width >= 1280 ? 4 : 3;
+
   return (
     <section>
-      <div>
-        {' '}
-        {partners.map(({ id, img, name }) => (
-          <PartnerCard key={id} img={img} name={name} />
-        ))}
+      <div className='container pt-[60px] md:pt-[100px] xl:pt-[130px]'>
+        <div className='mb-[40px] flex md:mb-[50px] xl:mb-[60px]'>
+          <h2 className='xl:text-h2_desc text-h2 md:text-h2_tab'>Партнери</h2>
+        </div>
+
+        <ul className='flex flex-wrap gap-[10px] md:gap-[24px] md:gap-[25px]'>
+          {partners.slice(0, numberOfCards).map(({ id, img, name }) => (
+            <li key={id}>
+              <PartnerCard img={img} name={name} />
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
