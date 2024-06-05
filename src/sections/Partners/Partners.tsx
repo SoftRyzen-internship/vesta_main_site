ceea864af7614da10ddf44ad0d204deae3fcd175
Violeta <vnaboishchykova@gmail.com>
2024-06-05 18:49:00 +0200

    added_linkButton

--- src/sections/Partners/Partners.tsx
+++ src/sections/Partners/Partners.tsx
@@ -4,6 +4,7 @@ import { FC } from 'react';
 import { usePathname } from 'next/navigation';
 
 import { PartnerCard } from '@/components/common/PartnerCard';
+import { LinkButton } from '@/components/ui/LinkButton';
 
 import { useWindowSize } from '@/utils/getWindowSize';
 import { partners } from '@/data';
@@ -15,10 +16,19 @@ export const Partners: FC = () => {
     pathName === '/about' ? 12 : sizes.width && sizes.width >= 1280 ? 4 : 3;
 
   return (
-    <section>
+    <section id='partners'>
       <div className='container pt-[60px] md:pt-[100px] xl:pt-[130px]'>
-        <div className='mb-[40px] flex md:mb-[50px] xl:mb-[60px]'>
+        <div className='mb-[40px] flex  flex-col gap-[24px] md:mb-[50px] xl:mb-[60px] xl:flex-row xl:justify-between'>
           <h2 className='xl:text-h2_desc text-h2 md:text-h2_tab'>Партнери</h2>
+          {pathName !== '/about' && (
+            <LinkButton
+              variant='secondary'
+              href='/about#partners'
+              className='h-[28px] w-[125px]'
+            >
+              дивитись всі
+            </LinkButton>
+          )}
         </div>
 
         <ul className='flex flex-wrap gap-[10px] md:gap-[24px] md:gap-[25px]'>
