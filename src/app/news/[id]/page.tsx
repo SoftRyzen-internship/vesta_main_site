3d3e71c2a4456eb30f550d5e41c04a08fbbc9fe2
Alina-Pishak <alinapishak@icloud.com>
2024-06-06 18:42:32 +0300

    add mobile evrsion

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -1,9 +1,55 @@
 import { News } from '@/sections/News';
 
-export default function Page() {
+import { news } from '@/data';
+import Image from 'next/image';
+import clsx from 'clsx';
+
+export default function Page({ params }: { params: { id: string } }) {
+  const oneNews = news.find(({ id }) => id === params.id);
+
+  console.log(oneNews);
   return (
     <section>
       <div className='container'>
+        {oneNews && (
+          <div className='pt-[141px]'>
+            <p className='paragraph mb-[30px]'>{oneNews.date}</p>
+            <h2 className='mb-[50px] text-h3 text-green'>{oneNews.title}</h2>
+            <Image
+              src={oneNews.img}
+              alt={oneNews.imgAlt}
+              width={328}
+              height={260}
+              className='mb-10 h-[260px] w-full object-cover'
+            />
+            <p className={clsx('paragraph', oneNews.video && 'mb-10')}>
+              {oneNews.description}
+            </p>
+
+            {oneNews.video && (
+              <video
+                width='328'
+                height='260'
+                controls
+                preload='none'
+                className='mb-10'
+              >
+                <source src='/path/to/video.mp4' type='video/mp4' />
+                <track
+                  src='/path/to/captions.vtt'
+                  kind='subtitles'
+                  srcLang='en'
+                  label='English'
+                />
+                Your browser does not support the video tag.
+              </video>
+            )}
+            {oneNews.videoDescription && (
+              <p className='paragraph'> {oneNews.videoDescription}</p>
+            )}
+          </div>
+        )}
+
         <News />
       </div>
     </section>
