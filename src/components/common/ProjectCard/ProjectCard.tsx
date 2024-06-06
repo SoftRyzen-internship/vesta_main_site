ad3a23bf22e29397dbca41da73b39c8525d01bb7
Alina-Pishak <alinapishak@icloud.com>
2024-06-06 12:05:42 +0300

    fix images

--- src/components/common/ProjectCard/ProjectCard.tsx
+++ src/components/common/ProjectCard/ProjectCard.tsx
@@ -28,10 +28,10 @@ export const ProjectCard: FC<IProjectCardProps> = ({
       <Image
         src={img}
         alt={imgAlt}
-        width={0}
-        height={0}
-        sizes='100vw'
-        className='mb-[30px] w-full object-cover object-center md:mb-0 md:h-[250px] md:w-[300px] xl:h-[300px] xl:w-[492px]'
+        width={492}
+        height={300}
+        sizes='(min-width: 768px) 300px, (min-width: 1280px) 492px, 100%'
+        className='mb-[30px] h-[250px] w-full object-cover object-center md:mb-0 md:h-[250px] md:w-[300px] xl:h-[300px] xl:w-[492px]'
       />
       <div>
         <h2 className='mb-5 line-clamp-2 font-kyiv text-h3 font-bold text-green text-inherit md:mb-10 md:text-h3_tab xl:text-h3_desk'>
