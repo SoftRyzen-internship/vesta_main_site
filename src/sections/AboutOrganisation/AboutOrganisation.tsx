8ea3da97f0a05d4c5a24b1a98d9d0888cced02dc
Anatoliy Daniluyk <anatolydanilyuk2002@gmail.com>
2024-06-07 13:54:26 +0300

    add scroll for section about

--- src/sections/AboutOrganisation/AboutOrganisation.tsx
+++ src/sections/AboutOrganisation/AboutOrganisation.tsx
@@ -1,9 +1,8 @@
 import { FC } from 'react';
-
 import { SupportCards } from '@/components/common/SupportCards';
-
 import { supportCards, aboutOrganisation } from '@/data';
 import { getSpecialWords } from '@/utils';
+import { ScrollBox } from '@/components/ui/ScrollBox';
 
 export const AboutOrganisation: FC = () => {
   const { caption, title, titleText, refreshData } = aboutOrganisation;
@@ -17,7 +16,7 @@ export const AboutOrganisation: FC = () => {
         <div className='xl:flex xl:flex-col'>
           <h3
             className='my-[30px] font-kyiv text-h3 font-bold text-green 
-        transition md:w-[596px] md:text-h3_tab xl:mb-[20px] xl:mt-0 xl:w-[700px] xl:text-h3_desk'
+            transition md:w-[596px] md:text-h3_tab xl:mb-[20px] xl:mt-0 xl:w-[700px] xl:text-h3_desk'
           >
             {getSpecialWords(title, 0, 8, { end: true })}
             <span className='bg-yellowLight'>
@@ -32,21 +31,23 @@ export const AboutOrganisation: FC = () => {
       </div>
       <div className='container'>
         <p
-          className='pb-[10px] text-body4 font-normal text-darkGrey transition
-        xl:flex xl:justify-end'
+          className=' pb-[10px] text-body4 font-normal text-darkGrey transition
+          xl:flex xl:justify-end'
         >
           {refreshData}
         </p>
-        <ul className='flex gap-5 overflow-x-auto whitespace-nowrap transition'>
-          {supportCards.map(card => (
-            <SupportCards
-              key={card.id}
-              id={card.id}
-              amountOfHelp={card.amountOfHelp}
-              typeOfHelp={card.typeOfHelp}
-            />
-          ))}
-        </ul>
+        <ScrollBox className='overflow-x-auto'>
+          <ul className='flex gap-5 transition pb-10'>
+            {supportCards.map(card => (
+              <SupportCards
+                key={card.id}
+                id={card.id}
+                amountOfHelp={card.amountOfHelp}
+                typeOfHelp={card.typeOfHelp}
+              />
+            ))}
+          </ul>
+        </ScrollBox>
       </div>
     </section>
   );
