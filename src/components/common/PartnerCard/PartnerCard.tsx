29e66ebe0e8e68575a9e2ec72ef693d467510c0d
Violeta <vnaboishchykova@gmail.com>
2024-06-11 11:45:37 +0200

    fixed_props

--- src/components/common/PartnerCard/PartnerCard.tsx
+++ src/components/common/PartnerCard/PartnerCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 import { IPartnerCardProps } from './PartnerCard.types';
 
-export const PartnerCard: FC<IPartnerCardProps> = ({ src, name }) => {
+export const PartnerCard: FC<IPartnerCardProps> = ({ item: { src, name } }) => {
   return (
     <div className='flex h-[263px] w-[250px] flex-col justify-end bg-bgText p-5 pt-[67px] md:w-[212px] md:pt-[74px] xl:h-[294px] xl:w-[285px] xl:pt-[60px]'>
       <Image
