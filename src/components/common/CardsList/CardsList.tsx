6f867dd39590533cf76f2335948aeabf8469569a
Ljuzifer <Ljuzifer@gmail.com>
2024-06-11 03:28:51 +0300

    fixes around all project

--- src/components/common/CardsList/CardsList.tsx
+++ src/components/common/CardsList/CardsList.tsx
@@ -26,7 +26,7 @@ export const CardsList: FC<CardListProps> = ({
       <ul
         className={clsx(
           pathname === '/about' && 'mb-0',
-          'mb-[60px] flex gap-2.5 overflow-x-auto scrollbar scrollbar-track-bgText scrollbar-thumb-green scrollbar-track-rounded-full scrollbar-thumb-rounded-full smOnly:pb-10 md:mb-0 md:flex-wrap md:gap-6 xl:gap-6',
+          'mb-[60px] flex gap-2.5 overflow-x-auto scrollbar scrollbar-track-bgText scrollbar-thumb-green scrollbar-track-rounded-full scrollbar-thumb-rounded-full smOnly:pb-10 md:mb-0 md:flex-wrap md:gap-6 xl:w-[1215px] xl:gap-6',
         )}
       >
         {items.map(
