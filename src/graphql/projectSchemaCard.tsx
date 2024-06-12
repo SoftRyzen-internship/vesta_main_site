3e9bf1ced9e9688b1edc7a498d9e4039c0b48ad5
Symeon Melnychuk <m.simeon998@gmail.com>
2024-06-12 11:07:05 +0300

    fix2

--- src/graphql/projectSchemaCard.tsx
+++ src/graphql/projectSchemaCard.tsx
@@ -1,6 +1,6 @@
 import { gql } from 'graphql-request';
 
-export const getProjects = gql`
+export const getProjectsCard = (start: number, limit: number) => gql`
 query {
   projects(
     pagination: { start: ${start}, limit: ${limit} },
