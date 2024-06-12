6a29bf0405246f7745844cba026665ef055a0aa7
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-06-12 15:08:49 +0300

    Added fetching data for Contacts

--- src/actions/fetchData.ts
+++ src/actions/fetchData.ts
@@ -1,9 +1,12 @@
 import { request } from 'graphql-request';
 
-export const fetchData = async (query: string) => {
+export const fetchData = async <T>(query: string): Promise<T> => {
   try {
-    const data = await request('https://vesta-admin.onrender.com/graphql', query);
-    return data;
+    const data = await request(
+      'https://vesta-admin.onrender.com/graphql',
+      query,
+    );
+    return data as T;
   } catch (error) {
     console.error('Error:', error);
     throw error;
