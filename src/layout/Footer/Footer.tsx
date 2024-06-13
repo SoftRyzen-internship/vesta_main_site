1027800e39d977b73b93cbfb48bf2aca0a78f79d
Anatoliy Daniluyk <anatolydanilyuk2002@gmail.com>
2024-06-13 21:53:14 +0300

    fix async fetch

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,33 +1,43 @@
 'use client';
-import { FC } from 'react';
-
+import React, { FC, useEffect, useState } from 'react';
 import { Logo } from '@/components/ui/Logo';
 import { SocialMedia } from '@/components/ui/SocialMedia';
 import { NavList } from '@/components/common/NavList';
-
-import { socialMedia, footer, footerNavigation} from '@/data';
-
+import { socialMedia, footer, footerNavigation } from '@/data';
 import { scrollToTop } from '@/utils';
-
 import SoftRyzen from '/public/icons/softryzen.svg';
 import GoIt from '/public/icons/goIt.svg';
 import ScrollUp from '/public/icons/scrollUp.svg';
 import { fetchData } from '@/actions/fetchData';
-
 import { getContact } from '@/graphql/contactSchema';
 import { FooterOrganizationResponse, FooterPhones } from './Footer.types';
 
-export const Footer: FC = async () => {
-  const data: FooterOrganizationResponse = await fetchData<FooterOrganizationResponse>(getContact);
+export const Footer: FC = () => {
+  const [supportCards, setSupportCards] = useState<FooterPhones[]>([]);
+  const [email, setEmail] = useState<string>('');
 
-  const { legal_support, psychological_support, head_organization, email } =
-  data.contact.data.attributes;
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      try {
+        const data: FooterOrganizationResponse = await fetchData<FooterOrganizationResponse>(getContact);
+        const { legal_support, psychological_support, head_organization, email } = data.contact.data.attributes;
 
-const updatedSupportCards: FooterPhones[] = [
-  { id: 1, typeOfHelp: 'Юридичні послуги', aria: 'Зателефонувати за юридичною допомогою', phone: legal_support },
-  { id: 2, typeOfHelp: 'Психологічні послуги:', aria: 'Зателефонувати за психологічною допомогою', phone: psychological_support },
-  { id: 3, typeOfHelp: 'Зв\'язатися за нами', aria: 'Зателефонувати організції', phone: head_organization }
-];
+        const updatedSupportCards: FooterPhones[] = [
+          { id: 1, typeOfHelp: 'Юридичні послуги', aria: 'Зателефонувати за юридичною допомогою', phone: legal_support },
+          { id: 2, typeOfHelp: 'Психологічні послуги:', aria: 'Зателефонувати за психологічною допомогою', phone: psychological_support },
+          { id: 3, typeOfHelp: 'Зв\'язатися за нами', aria: 'Зателефонувати організції', phone: head_organization }
+        ];
+
+        setSupportCards(updatedSupportCards);
+        setEmail(email);
+      } catch (error) {
+        console.error('Error fetching footer data:', error);
+      }
+    };
+
+    fetchDataAsync();
+  }, []);
+
   return (
     <footer className='bg-green'>
       <div className='container pb-[15px] pt-[50px] text-white transition md:pb-[23px] md:pt-[60px] xl:pb-[28px]'>
@@ -42,7 +52,7 @@ const updatedSupportCards: FooterPhones[] = [
                 </button>
               </div>
               <div className='flex flex-col gap-[15px] pb-10 text-body4 font-normal text-white transition md:pb-[96px] md:pl-[169px] md:pt-[40px] xl:pb-[100px] xl:pl-[236px] xl:pt-0'>
-                {updatedSupportCards.map(({ id, typeOfHelp,aria, phone }) => (
+                {supportCards.map(({ id, typeOfHelp, aria, phone }) => (
                   <div key={id} className='flex flex-col gap-1'>
                     <p className='text-greenHover text-whiteGrey'>
                       {typeOfHelp}
