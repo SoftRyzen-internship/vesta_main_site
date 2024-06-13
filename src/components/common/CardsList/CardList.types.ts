eb335dd129fa3ffb166a52a0c3b5e57a312b32f7
Violeta <vnaboishchykova@gmail.com>
2024-06-13 18:37:58 +0200

    fixed

--- src/components/common/CardsList/CardList.types.ts
+++ src/components/common/CardsList/CardList.types.ts
@@ -1,6 +1,6 @@
 import { ElementType } from 'react';
 import { IItem } from '@/sections/Partners/Partners.types';
-import { IItemTeam } from '@/sections/Team/Team.styles';
+import { IItemTeam } from '@/sections/Team/Team.types';
 
 export interface CardListProps {
   items: IItem[] | IItemTeam[];
