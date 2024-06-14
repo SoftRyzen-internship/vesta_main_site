5a2947bc9e4eef45a64e821a1523fc9a6d0ac23a
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-06-14 17:49:34 +0300

    Finished without socials

--- src/sections/pageProjects/ProjectTemplate/ProjectTemplate.tsx
+++ src/sections/pageProjects/ProjectTemplate/ProjectTemplate.tsx
@@ -1,30 +1,24 @@
 import { FC } from 'react';
-
 import Image from 'next/image';
 
-import { SocialMedia } from '@/components/ui/SocialMedia';
+import { IOneProjectsProps } from './ProjectTemplate.types';
 
-import { projectTemplateData } from '@/data';
-
-export const ProjectTemplate: FC = () => {
-  const {
-    full: { title, description, socials, img, imgAlt, subtitle, text },
-  } = projectTemplateData;
+export const ProjectTemplate: FC<IOneProjectsProps> = ({ oneProject }) => {
+  const { title, pageText, add_title, add_text, image } = oneProject;
 
   return (
     <section className='pb-[60px] pt-[134px] md:pb-[100px] md:pt-[154px] xl:pb-[130px] xl:pt-[191px]'>
       <div className='container'>
         <div className='justify-between xl:flex'>
-          <div className='mb-[30px]  md:mb-10 md:max-w-[492px] xl:mb-0'>
+          <div className='mb-[30px] md:mb-10 md:max-w-[492px] xl:mb-0'>
             <h1 className='subtitle'>{title}</h1>
-            <p className='mt-5 pb-10 text-body3 text-darkGrey xl:text-body3_desk'>
-              {description}
+            <p className='mt-5 whitespace-pre-wrap pb-10 text-body3 text-darkGrey xl:text-body3_desk'>
+              {pageText}
             </p>
-            <SocialMedia lightMode={false} socialMedia={socials} />
           </div>
           <Image
-            src={img}
-            alt={imgAlt}
+            src={image.data.attributes.url}
+            alt={image.data.attributes.alternativeText}
             width={448}
             height={232}
             className='h-[232px] w-full object-cover md:h-[366px] md:w-[684px] xl:w-[595px]'
@@ -32,14 +26,14 @@ export const ProjectTemplate: FC = () => {
         </div>
 
         <div className='mt-[60px] justify-between md:mt-[100px] xl:mt-[130px] xl:flex '>
-          {subtitle ? (
-            <h2 className='text-body1 md:max-w-[566px] md:text-body1_tab xl:text-body1_desk xl:max-w-[492px]'>
-              {subtitle}
+          {add_title ? (
+            <h2 className='text-body1 md:max-w-[566px] md:text-body1_tab xl:max-w-[492px] xl:text-body1_desk'>
+              {add_title}
             </h2>
           ) : null}
-          {text ? (
-            <p className='paragraph md:max-w-[566px] xl:max-w-[595px] notXl:mt-10'>
-              {text}
+          {add_text ? (
+            <p className='paragraph whitespace-pre-wrap md:max-w-[566px] xl:max-w-[595px] notXl:mt-10'>
+              {add_text}
             </p>
           ) : null}
         </div>
---
+++ src/sections/pageProjects/ProjectTemplate/ProjectTemplate.types.ts
@@ -0,0 +1,56 @@
+export interface ISocialData {
+  link: string;
+  social: string;
+}
+
+export interface IImageAttributes {
+  url: string;
+  alternativeText: string;
+}
+
+export interface IImageData {
+  attributes: IImageAttributes;
+}
+
+export interface IProjectImageData {
+  data: IImageData;
+}
+
+export interface IProjectAttributes {
+  publishedAt: string;
+  title: string;
+  description: string;
+  pageText: string;
+  add_title: string;
+  add_text: string;
+  slug: string;
+  image: IProjectImageData;
+  social: ISocialData[];
+}
+
+export interface IProjectData {
+  id: string;
+  attributes: IProjectAttributes;
+}
+
+export interface IProject {
+  data: IProjectData[];
+}
+
+export interface IProjectsData {
+  projects: IProject;
+}
+
+export interface IOneProjectsProps {
+  oneProject: {
+    publishedAt: string;
+    title: string;
+    description: string;
+    pageText: string;
+    add_title: string;
+    add_text: string;
+    slug: string;
+    image: IProjectImageData;
+    social: ISocialData[];
+  };
+}
