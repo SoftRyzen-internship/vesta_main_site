63deb27aae1677dcdcfb9b7ff2a3467c13b204b1
VioletaNaboy <121202853+VioletaNaboy@users.noreply.github.com>
2024-06-18 11:05:43 +0300

    teamCard-props

--- src/components/common/CardsList/CardList.types.ts
+++ src/components/common/CardsList/CardList.types.ts
@@ -3,21 +3,8 @@ import { IItem } from '@/sections/Partners/Partners.types';
 import { IItemTeam } from '@/sections/Team/Team.types';
 
 export interface CardListProps {
-<<<<<<< HEAD
   items: IItem[] | IItemTeam[];
-=======
-  items: {
-    id: number;
-    src: string;
-    name: string;
-    alt?: string;
-    position?: string;
-    description?: string;
-    plug?: string;
-    socialMedia?: { social: string; link: string }[];
-  }[];
->>>>>>> origin/dev
   CardComponent: ElementType;
   path: string;
-  section: string;
+  section: string
 }
