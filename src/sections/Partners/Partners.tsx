874c0445170e623da42d642397f77909d426c82d
Ljuzifer <Ljuzifer@gmail.com>
2024-06-20 01:40:12 +0300

    add no-data for team & partners

--- src/sections/Partners/Partners.tsx
+++ src/sections/Partners/Partners.tsx
@@ -9,15 +9,18 @@ import { LinkButton } from '@/components/ui/LinkButton';
 
 import { PartnersProps } from './Partners.types';
 
-import { partnersData } from '@/data';
+import { partnersData, templateNoData } from '@/data';
 
 import { useWindowSize } from '@/utils';
+import { NoDataTemplate } from '../NoDataTemplate';
 
 export const Partners: FC<PartnersProps> = ({ partners }) => {
   const [partnersForRender, setPartners] = useState(partners);
   const { width } = useWindowSize();
   const pathName = usePathname();
   const isDesktop = width && width > 1279 ? true : false;
+  const { sectionPartners, titlePartners, descriptionPartners } =
+    templateNoData;
 
   useEffect(() => {
     if (!isDesktop && pathName !== '/about') {
@@ -28,29 +31,42 @@ export const Partners: FC<PartnersProps> = ({ partners }) => {
   }, [isDesktop, pathName, partners]);
 
   return (
-    <section id='partners' className='pt-[60px] md:pt-[100px] xl:pt-[130px]'>
-      <div className='container'>
-        <div className='mb-[40px] flex flex-col gap-[24px] md:mb-[50px] xl:mb-[60px] xl:flex-row xl:justify-between'>
-          <h2 className='xl:text-h2_desc font-kyiv text-h2 md:text-h2_tab'>
-            {partnersData.title}
-          </h2>
-          {pathName !== '/about' && (
-            <LinkButton
-              variant='secondary'
-              href='/about#partners'
-              className='h-[28px] w-[125px]'
-            >
-              {partnersData.button}
-            </LinkButton>
-          )}
-        </div>
-        <CardsList
-          items={partnersForRender}
-          CardComponent={PartnerCard}
-          path={pathName}
-          section='partners'
+    <>
+      {partners.length === 0 ? (
+        <NoDataTemplate
+          sectionTitle={sectionPartners}
+          title={titlePartners}
+          description={descriptionPartners}
         />
-      </div>
-    </section>
+      ) : (
+        <section
+          id='partners'
+          className='pt-[60px] md:pt-[100px] xl:pt-[130px]'
+        >
+          <div className='container'>
+            <div className='mb-[40px] flex flex-col gap-[24px] md:mb-[50px] xl:mb-[60px] xl:flex-row xl:justify-between'>
+              <h2 className='xl:text-h2_desc font-kyiv text-h2 md:text-h2_tab'>
+                {partnersData.title}
+              </h2>
+              {pathName !== '/about' && (
+                <LinkButton
+                  variant='secondary'
+                  href='/about#partners'
+                  className='h-[28px] w-[125px]'
+                >
+                  {partnersData.button}
+                </LinkButton>
+              )}
+            </div>
+            <CardsList
+              items={partnersForRender}
+              CardComponent={PartnerCard}
+              path={pathName}
+              section='partners'
+            />
+          </div>
+        </section>
+      )}
+    </>
   );
 };
--- src/sections/Team/Team.tsx
+++ src/sections/Team/Team.tsx
@@ -10,33 +10,45 @@ import { TeamCard } from '@/components/common/TeamCard';
 
 import { TeamProps } from './Team.types';
 
-import { teamData } from '@/data';
+import { teamData, templateNoData } from '@/data';
+import { NoDataTemplate } from '../NoDataTemplate';
 
 export const Team: FC<TeamProps> = ({ team }) => {
+  const { sectionTeam, emptyTitle, emptyDescription } = templateNoData;
   const pathName = usePathname();
 
   return (
-    <section
-      id='team'
-      className={clsx(
-        pathName === '/about' && 'smOnly:pb-0',
-        pathName !== '/about' && 'pb-[60px] md:pb-[100px] xl:pb-[130px]',
-        'md:pt-[100px] xl:pt-[130px]',
-      )}
-    >
-      <div className='container'>
-        <div className='mb-[40px] flex  flex-col gap-[24px] md:mb-[50px] xl:mb-[60px] xl:flex-row xl:justify-between'>
-          <h2 className='xl:text-h2_desc font-kyiv text-h2 md:text-h2_tab'>
-            {teamData.title}
-          </h2>
-        </div>
-        <CardsList
-          items={team}
-          CardComponent={TeamCard}
-          path={pathName}
-          section='team'
+    <>
+      {team.length === 0 ? (
+        <NoDataTemplate
+          sectionTitle={sectionTeam}
+          title={emptyTitle}
+          description={emptyDescription}
         />
-      </div>
-    </section>
+      ) : (
+        <section
+          id='team'
+          className={clsx(
+            pathName === '/about' && 'smOnly:pb-0',
+            pathName !== '/about' && 'pb-[60px] md:pb-[100px] xl:pb-[130px]',
+            'md:pt-[100px] xl:pt-[130px]',
+          )}
+        >
+          <div className='container'>
+            <div className='mb-[40px] flex  flex-col gap-[24px] md:mb-[50px] xl:mb-[60px] xl:flex-row xl:justify-between'>
+              <h2 className='xl:text-h2_desc font-kyiv text-h2 md:text-h2_tab'>
+                {teamData.title}
+              </h2>
+            </div>
+            <CardsList
+              items={team}
+              CardComponent={TeamCard}
+              path={pathName}
+              section='team'
+            />
+          </div>
+        </section>
+      )}
+    </>
   );
 };
