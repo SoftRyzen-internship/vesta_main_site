e4e68c0db0481af58fa4493be6d93b3331d41798
Ljuzifer <Ljuzifer@gmail.com>
2024-06-21 12:23:02 +0300

    add admin-url to env & env to gitignor & check for image-url/alt in team card

--- src/actions/fetchData.ts
+++ src/actions/fetchData.ts
@@ -1,11 +1,10 @@
 import { request } from 'graphql-request';
 
+const BASE_ADMIN_URL = process.env.NEXT_PUBLIC_BASE_ADMIN_URL as string;
+
 export const fetchData = async <T>(query: string): Promise<T> => {
   try {
-    const data = await request<T>(
-      'https://vesta-admin.onrender.com/graphql',
-      query,
-    );
+    const data = await request<T>(BASE_ADMIN_URL, query);
     return data;
   } catch (error) {
     console.error('Error:', error);
